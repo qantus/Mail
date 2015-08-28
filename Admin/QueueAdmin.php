31f5af1b0bfed2b9678df9c8a0ea9d798f8bb8bb
Max107 <max@studio107.ru>
2015-08-28 13:19:12 +0300

    fixes

--- Admin/QueueAdmin.php
+++ Admin/QueueAdmin.php
@@ -9,10 +9,16 @@
 namespace Modules\Mail\Admin;
 
 use Modules\Admin\Components\ModelAdmin;
+use Modules\Mail\Forms\QueueForm;
 use Modules\Mail\Models\Queue;
 
 class QueueAdmin extends ModelAdmin
 {
+    public function getCreateForm()
+    {
+        return QueueForm::className();
+    }
+
     public function getColumns()
     {
         return ['name', 'subject', 'created_at', 'user', 'count', 'is_complete', 'started_at', 'stopped_at'];
